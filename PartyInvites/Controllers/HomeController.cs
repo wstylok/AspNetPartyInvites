eba4e84863fef3b8b5457fc598a8c3a32e2169b9
Wojciech Stylok <wojciech@stylok.pl>
2019-07-02 16:46:52 +0200

    adding response to Repository

--- PartyInvites/Controllers/HomeController.cs
+++ PartyInvites/Controllers/HomeController.cs
@@ -8,11 +8,13 @@ namespace PartyInvites.Controllers
     {
         public ViewResult Index()
         {
+            //greetings and current time info
             int hour = DateTime.Now.Hour;
             ViewBag.Greeting = hour < 17 ? "Dzień dobry" : "Dobry wieczór";
             ViewBag.Hour = $"{DateTime.Now.Hour}:{DateTime.Now.Minute}";
             ViewBag.Day = $"{DateTime.Now.Day}-{DateTime.Now.Month}-{DateTime.Now.Year}";
 
+            //days left to party
             int daysLeft = 27 - DateTime.Now.Day;
             string d = daysLeft != 1 ? "dni" : "dzień";
             ViewBag.TimeLeft = daysLeft >= 0 ? $"Do urodzin zostało: {daysLeft} {d}" : "W tym roku już po imprezie, zapraszamy za rok :-)";
@@ -30,7 +32,10 @@ namespace PartyInvites.Controllers
         public ViewResult RsvpForm(GuestResponse guestResponse)
         {
             //TODO: wysyłanie maila do organizatora
-            return View();
+
+            //adding response to Repository
+            Repository.AddResponse(guestResponse);
+            return View("Thanks", guestResponse);
         }
     }
 }
